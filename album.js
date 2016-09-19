982811db33ced3efbb5bb7aff29757b1039c8a0e
Cam Crews <camcrews@gmail.com>
2016-09-19 12:12:37 -0400

    listPhotos method

--- album.js
+++ album.js
@@ -2,6 +2,29 @@
 // Our Model...
 // has a <file location>, <location where taken>
 
+function Album(){
+  this.photos = [];
+  //add a new photo,
+  this.addPhoto = function( file, location ) {
+    //to do: finish this
+    this.photos.push( new Photo(file, location) );
+    return this;
+  };
+  //list all photos, and 
+  this.listPhotos = function() {
+    //to do: write the list
+    var output = "";
+    this.photos.forEach(function(photo){
+      output += photo.file + " [" + photo.location + "],";
+    });
+    return output.slice(0,-1);
+  };
+  //access a specific photo
+  this.getPhoto = function( i ) {
+    //to do: return photo
+  }
+}
+
 function Photo( file, location ) {
   this.file = file;
   this.location = location;
@@ -14,11 +37,16 @@ function Photo( file, location ) {
 // var photo2 = new Photo("img/bubble_dog.jpg","Philadelphia");
 // var photo3 = new Photo("img/duck.jpg","Albany");
 
-var photos = [
-  new Photo("img/space_tiger.jpg","New York"),
-  new Photo("img/bubble_dog.jpg","Philadelphia"),
-  new Photo("img/duck.jpg","Albany")
-];
+// var photos = [
+//   new Photo("img/space_tiger.jpg","New York"),
+//   new Photo("img/bubble_dog.jpg","Philadelphia"),
+//   new Photo("img/duck.jpg","Albany")
+// ];
+
+var myAlbum = new Album();
+// we can method chain by returning "this" from
+// the method we're calling...
+myAlbum.addPhoto("img/space_tiger.jpg","New York").addPhoto("img/bubble_dog.jpg","Philadelphia").addPhoto("img/duck.jpg","Albany");
 
 document.addEventListener("DOMContentLoaded",function(){
   var elAlbum = document.getElementById("album");
@@ -26,9 +54,10 @@ document.addEventListener("DOMContentLoaded",function(){
   // for(var i = 0; i < photos.length; i++){
   //   elAlbum.innerHTML += photos[i].toHTML();
   // }
-  photos.forEach(function(photo){
+  myAlbum.photos.forEach(function(photo){
     elAlbum.innerHTML += photo.toHTML();
   });
+  console.log( myAlbum.listPhotos() );
 });
 
 // var photo1 = {
